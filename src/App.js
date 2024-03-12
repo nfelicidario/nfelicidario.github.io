1c3faaf4e8396495bca2539bc5e2a31a84d7f7a4
Nolan Felicidario <nolanfelicidario@nolans-mbp-2.myfiosgateway.com>
2024-03-12 05:24:24 -0400

    Cleanup unnecessary files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Panel from './Panel';
 
 const App = () => {
   return (
+    
     <div className="container mx-auto py-8">
       <Panel title="Example Panel" children="test">
         <p>This is the content of the panel.</p>
